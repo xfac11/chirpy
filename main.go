68108026c731341f22a7b880dd260dca517424f2
xfac11 <xfac115@gmail.com>
2026-06-15 10:34:37 +0200

    fileserver which uses '/app/' as prefix. healthz endpoint to see if the server is up and running.

--- main.go
+++ main.go
@@ -5,8 +5,20 @@ import (
 	"net/http"
 )
 
+func healthzHandler(responseWrite http.ResponseWriter, request *http.Request) {
+	responseWrite.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	responseWrite.WriteHeader(http.StatusOK)
+
+	var body = []byte("OK")
+	responseWrite.Write(body)
+}
 func main() {
+	fileServer := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
+
 	serveMux := http.NewServeMux()
+	serveMux.Handle("GET /app/", fileServer)
+	serveMux.HandleFunc("/healthz", healthzHandler)
+
 	server := http.Server{
 		Addr:    ":8080",
 		Handler: serveMux,
