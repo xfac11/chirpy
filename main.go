581311c4a6c0f1d9df6afa5cf4e2ff17bdd71f9b
xfac11 <xfac115@gmail.com>
2026-06-22 10:02:17 +0200

    Validate chirp for 140 characters max and replacing bad words

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 	"sync/atomic"
 )
 
@@ -19,7 +21,7 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func (cfg *apiConfig) metricsHandler(responseWrite http.ResponseWriter, request *http.Request) {
-	responseWrite.Header().Add("Content-Type", "text/html; charset=utf-8")
+	responseWrite.Header().Set("Content-Type", "text/html; charset=utf-8")
 	responseWrite.WriteHeader(http.StatusOK)
 
 	requestsText := fmt.Sprintf("<html><body><h1>Welcome, Chirpy Admin</h1><p>Chirpy has been visited %d times!</p></body></html>", cfg.fileserverHits.Load())
@@ -30,7 +32,7 @@ func (cfg *apiConfig) metricsHandler(responseWrite http.ResponseWriter, request 
 func (cfg *apiConfig) resetHandler(responeWrite http.ResponseWriter, request *http.Request) {
 	cfg.fileserverHits.Store(0)
 
-	responeWrite.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	responeWrite.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	responeWrite.WriteHeader(http.StatusOK)
 	var body = []byte("OK")
 	responeWrite.Write(body)
@@ -40,6 +42,97 @@ func middlewareLog(next http.Handler) http.Handler {
 		log.Printf("%s %s", r.Method, r.URL.Path)
 		next.ServeHTTP(w, r)
 	})
+}
+
+func writeJsondataError(text string) ([]byte, error) {
+	type returnVals struct {
+		Error string `json:"error"`
+	}
+	respBody := returnVals{
+		Error: text,
+	}
+	jsonData, err := json.Marshal(respBody)
+	return jsonData, err
+}
+
+func writeJsondataValid() ([]byte, error) {
+	type returnVals struct {
+		Valid bool `json:"valid"`
+	}
+	respBody := returnVals{
+		Valid: true,
+	}
+	jsonData, err := json.Marshal(respBody)
+	return jsonData, err
+}
+
+func validateChirpHandler(response http.ResponseWriter, request *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+
+	response.Header().Set("Content-Type", "application/json")
+	decoder := json.NewDecoder(request.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		log.Printf("Error decoding parameters: %s", err)
+		response.WriteHeader(http.StatusInternalServerError)
+		jsonData, err := writeJsondataError("Something went wrong")
+		if err != nil {
+			log.Printf("Error marshaling return values: %s", err)
+			response.Write([]byte("Error marshaling"))
+			return
+		}
+		response.Write(jsonData)
+		return
+	}
+
+	if len(params.Body) > 140 {
+		response.WriteHeader(http.StatusBadRequest)
+		jsonData, err := writeJsondataError("Chirp is too long")
+		if err != nil {
+			log.Printf("Error Marshaling response body: %s", err)
+			response.WriteHeader(http.StatusInternalServerError)
+			jsonData, err := writeJsondataError("Something went wrong")
+			if err != nil {
+				log.Printf("Error marshaling return values: %s", err)
+				response.Write([]byte("Error marshaling"))
+				return
+			}
+			response.Write(jsonData)
+			return
+		}
+		response.Write(jsonData)
+		return
+	}
+	splitBody := strings.Split(params.Body, "")
+	badWords := []string{"kerfuffle", "sharbert", "fornax"}
+	for i, word := range splitBody {
+		word = strings.ToLower(word)
+		for _, badWord := range badWords {
+			if strings.Contains(word, badWord) && len(word) == len(badWord) {
+				strings.ReplaceAll(word, badWord, "****")
+				splitBody[i] = word
+				break
+			}
+		}
+	}
+
+	type returnVals struct {
+		Valid bool `json:"valid"`
+	}
+	respBody := returnVals{
+		Valid: true,
+	}
+	jsonData, err := json.Marshal(respBody)
+	if err != nil {
+		log.Printf("Error marshaling valid response body: %s", err)
+		response.Write([]byte("Error marshaling"))
+	}
+	response.WriteHeader(http.StatusOK)
+	response.Write(jsonData)
+
 }
 
 func healthzHandler(responseWrite http.ResponseWriter, request *http.Request) {
@@ -60,6 +153,7 @@ func main() {
 	serveMux := http.NewServeMux()
 	serveMux.Handle("/app/", apiConfig.middlewareMetricsInc(middlewareLog(fileServer)))
 	serveMux.Handle("GET /api/healthz", middlewareLog(http.HandlerFunc(healthzHandler)))
+	serveMux.Handle("POST /api/validate_chirp", middlewareLog(http.HandlerFunc(validateChirpHandler)))
 	serveMux.Handle("GET /admin/metrics", middlewareLog(http.HandlerFunc(apiConfig.metricsHandler)))
 	serveMux.Handle("POST /admin/reset", middlewareLog(http.HandlerFunc(apiConfig.resetHandler)))
 
