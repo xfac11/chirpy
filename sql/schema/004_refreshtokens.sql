c5e5c2bd2795914c8cd465990d7e7fd10b5d9431
xfac11 <xfac115@gmail.com>
2026-07-06 21:11:24 +0200

    refreshtokens up/down migrations for allowing user to be logged in for longer periods

---
+++ sql/schema/004_refreshtokens.sql
@@ -0,0 +1,12 @@
+-- +goose Up
+CREATE TABLE refresh_tokens(
+    token TEXT PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    user_id UUID NOT NULL,
+    expires_at TIMESTAMP NOT NULL,
+    revoked_at TIMESTAMP,
+    FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+-- +goose Down
+DROP TABLE refresh_tokens;
