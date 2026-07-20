7587059dbe0c1d5a94c62af720e65970f73d7a86
xfac11 <xfac115@gmail.com>
2026-07-20 10:28:22 +0200

    adding a new chirp query that gets all chirps from a user sorted in ascending order

--- sql/queries/chirps.sql
+++ sql/queries/chirps.sql
@@ -20,3 +20,8 @@ WHERE id = $1;
 -- name: DeleteChirp :exec
 DELETE FROM chirps
 WHERE id = $1;
+
+-- name: GetAllChripsFromAuthor :many
+SELECT * FROM chirps
+WHERE user_id = $1
+ORDER BY created_at ASC;
